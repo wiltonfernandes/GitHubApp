1306f649b9780d9f3ce1fbe1323b5de614d6b1c3
Wilton Fernandes <84727834+wiltonfernandes@users.noreply.github.com>
2022-04-28 15:16:44 -0300

    setup ViewCode

--- GitHubApp/ViewController.swift
+++ GitHubApp/ViewController.swift
@@ -6,14 +6,16 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .red
         // Do any additional setup after loading the view.
     }
-
-
 }
 
+func setUpConstraints(){
+    
+}
---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '13.0'
+inhibit_all_warnings!
+
+target 'GitHubApp' do  
+  use_frameworks!
+
+  pod 'SwiftLint'
+  pod 'SnapKit', '~> 5.6'
+
+end
